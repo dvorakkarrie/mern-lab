a1f325fd9f3eb22873c1b67b44809b4287c6c6f8
Karrie Dvorak <dvorak.karrie@gmail.com>
2020-03-18 10:46:28 -0500

    Render the Users

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const backendUrl = "http://localhost:8080/api/users/";
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      name: ""
+    }
+  }
+
+  componentDidMount() {
+    axios({
+      method: 'GET',
+      url: backendUrl
+    })
+    .then(users => console.log(users))
+  }
+
+  render() {
+    return (
+      <div className="App">
+       
+      </div>
+    );
+  }
 }
 
 export default App;
